1b308f74a91022a2fb70014fdf852faf293e6004
kevbite <kev_bite@msn.com>
2019-04-30 20:47:25 +0100

    Adding in parkrun-selector.

--- src/selectors/parkrun-selector.spec.js
+++ src/selectors/parkrun-selector.spec.js
@@ -1,7 +1,42 @@
-import { selector } from './parkrun-selector.spec';
+import selectParkruns from './parkrun-selector';
 
-describe("selector", () => {
-  it("should fail at ci server", () => {
-    expect(true).toBe(true);
+describe("No filters", () => {
+  it("Should return all parkruns", () => {
+    const parkruns = [{ id: 1 }, { id: 2 }];
+    const filters = {};
+
+    const actual = selectParkruns({ parkruns, filters });
+
+    expect(actual).toEqual(parkruns);
+  });
+});
+
+describe("Wheelchair friendly filter", () => {
+  it("Should return only Wheelchair friendly parkruns", () => {
+    const parkruns = [
+      { id: 1, features: { wheelchairFriendly: true } },
+      { id: 2, features: { wheelchairFriendly: false } },
+      { id: 3, features: { wheelchairFriendly: undefined } }
+    ];
+    const filters = { wheelchairFriendly: true };
+
+    const actual = selectParkruns({ parkruns, filters });
+
+    expect(actual).toEqual([parkruns[0]]);
+  });
+});
+
+describe("Buggy friendly filter", () => {
+  it("Should return only buggy friendly parkruns", () => {
+    const parkruns = [
+      { id: 1, features: { buggyFriendly: true } },
+      { id: 2, features: { buggyFriendly: false } },
+      { id: 3, features: { buggyFriendly: undefined } }
+    ];
+    const filters = { buggyFriendly: true };
+
+    const actual = selectParkruns({ parkruns, filters });
+
+    expect(actual).toEqual([parkruns[0]]);
   });
 });
