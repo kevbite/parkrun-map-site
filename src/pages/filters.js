7a6c1eaf5fbc42990595b996f03c8dcff54335e6
kevbite <kev_bite@msn.com>
2019-04-30 22:09:59 +0100

    Start of filters.

--- src/pages/filters.js
+++ src/pages/filters.js
@@ -1,17 +1,63 @@
-import React, { useState } from "react"
-import { Helmet } from "react-helmet"
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Switch from '@material-ui/core/Switch';
+import ChildFriendlyIcon from '@material-ui/icons/ChildFriendly';
+import AccessibleForwardIcon from '@material-ui/icons/AccessibleForward';
+import BluetoothIcon from '@material-ui/icons/Bluetooth';
+import { useFilters } from '../filters/filters-context';
 
-export default () => {
-  const [count, setCount] = useState(0)
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    padding: 24,
+    justifyContent: 'center'
+  },
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+export default withStyles(styles)(({ classes }) => {
+  const { state: { filters }, setFilters } = useFilters();
+
+
+  const onFilterChanged = name => () => {
+    const value = !filters[name];
+    const newFilters = {
+      ...filters,
+    };
+    newFilters[name] = value;
+    setFilters(newFilters);
+  };
+
+  const availableFilters = [
+    { name: 'Wheelchair Friendly', propName: 'wheelchairFriendly', icon: <AccessibleForwardIcon /> },
+    { name: 'Buggy Friendly', propName: 'buggyFriendly', icon: <ChildFriendlyIcon /> }
+  ];
 
   return (
-    <div style={{ color: `purple` }}>
-      <Helmet>
-        <title>Filter parkruns</title>
-      </Helmet>
-      <h1>LOLZ FILTERS!!</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
+    <div className={classes.container}>
+      <List subheader={<h1>Filters</h1>} className={classes.root}>
+        {availableFilters.map(x => <ListItem onClick={onFilterChanged(x.propName)}>
+          <ListItemIcon>
+            {x.icon}
+          </ListItemIcon>
+          <ListItemText primary={x.name} />
+          <ListItemSecondaryAction>
+            <Switch
+              onChange={onFilterChanged(x.propName)}
+              checked={filters[x.propName]}
+            />
+          </ListItemSecondaryAction>
+        </ListItem>)}
+      </List>
     </div>
-  )
-}
+  );
+});
