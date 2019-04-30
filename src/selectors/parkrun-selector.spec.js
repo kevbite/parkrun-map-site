bf646a5242bda227927161d97c59f7c567adb3d9
kevbite <kev_bite@msn.com>
2019-04-30 20:36:39 +0100

    Making test pass

--- src/selectors/parkrun-selector.spec.js
+++ src/selectors/parkrun-selector.spec.js
@@ -2,6 +2,6 @@ import { selector } from './parkrun-selector.spec';
 
 describe("selector", () => {
   it("should fail at ci server", () => {
-    expect(false).toBe(true);
+    expect(true).toBe(true);
   });
 });
